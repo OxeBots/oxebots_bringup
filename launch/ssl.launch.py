608c0978e02fb633e76299bff4f6d5f9c1f2b275
Carlos Cerqueira <ce.cesc01@gmail.com>
2024-07-02 21:41:14 -0300

    feat: add ssl initial launcher

---
+++ launch/ssl.launch.py
@@ -0,0 +1,39 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    sender_node = Node(
+        package="oxebots_comms",
+        executable="sender_node",
+        name="sender_bridge_node",
+        parameters=[
+            {
+                "port": 10301
+            }
+        ]
+    )
+
+    game_receiver_node = Node(
+        package="oxebots_comms",
+        executable="game_receiver_node",
+        name="game_receiver_node",
+        parameters=[
+            {
+                "port": 10020
+            },
+            {
+                "robot_amount": 3
+            },
+            {
+                "is_yellow_team": True
+            }
+        ]
+    )
+
+    ld.add_action(game_receiver_node)
+    ld.add_action(sender_node)
+
+    return ld
