8e02bb8544aaf4b028248d901951afb21cfb1d6a
Erick <ericksuzart@gmail.com>
2025-09-24 00:19:42 -0300

    feat: add config for parameters && grSim node

--- launch/ssl.launch.py
+++ launch/ssl.launch.py
@@ -1,3 +1,6 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
@@ -5,47 +8,32 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = LaunchDescription()
 
-    sender_node = Node(
+    pkg_dir = get_package_share_directory("oxebots_bringup")
+    config_file = os.path.join(pkg_dir, "config", "ssl_config.yaml")
+
+    grSim_controller_node = Node(
         package="oxebots_comms",
-        executable="sender_node",
-        name="sender_bridge_node",
-        parameters=[
-            {
-                "port": 10301
-            }
-        ]
+        executable="grSim_controller_node",
+        name="grSim_controller_node",
+        parameters=[config_file],
     )
 
     game_receiver_node = Node(
         package="oxebots_comms",
         executable="game_receiver_node",
         name="game_receiver_node",
-        parameters=[
-            {
-                "port": 10020
-            },
-            {
-                "robot_amount": 3
-            },
-            {
-                "is_yellow_team": True
-            }
-        ]
+        parameters=[config_file],
     )
 
     game_observer_node = Node(
         package="oxebots_observers",
         executable="game_observer_node",
         name="game_observer_node",
-        parameters=[
-            {
-                "robot_amount": 3
-            }
-        ]
+        parameters=[config_file],
     )
 
     ld.add_action(game_receiver_node)
-    ld.add_action(sender_node)
+    ld.add_action(grSim_controller_node)
     ld.add_action(game_observer_node)
 
     return ld
