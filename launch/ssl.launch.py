eeb7521f26ac0470a4367a3335800ba466018f69
matheus <matheusrafael@ufba.br>
2025-10-11 00:55:43 +0000

    ultima att

--- launch/ssl.launch.py
+++ launch/ssl.launch.py
@@ -2,15 +2,20 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    pkg_dir = get_package_share_directory("oxebots_bringup")
-    config_file = os.path.join(pkg_dir, "config", "ssl_config.yaml")
+    # Paths
+    bringup_pkg_share = get_package_share_directory("oxebots_bringup")
+    strategy_pkg_share = get_package_share_directory("oxebots_strategy")
+    config_file = os.path.join(bringup_pkg_share, "config", "ssl_config.yaml")
 
+    # Communication and Observation Nodes
     grSim_controller_node = Node(
         package="oxebots_comms",
         executable="grSim_controller_node",
@@ -32,8 +37,16 @@ def generate_launch_description():
         parameters=[config_file],
     )
 
+    # Include Strategy Launch File
+    strategy_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(strategy_pkg_share, "launch", "strategy.launch.py")
+        )
+    )
+
     ld.add_action(game_receiver_node)
     ld.add_action(grSim_controller_node)
     ld.add_action(game_observer_node)
+    ld.add_action(strategy_launch)
 
     return ld
